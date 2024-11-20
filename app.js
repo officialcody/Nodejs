0012e0ba7f0a95d7bc89840578d0cfb02b21c951
Sparsh Goel <sparshgoelt26@gmail.com>
2024-11-21 01:15:11 +0530

    CommonJS Modules

---
+++ app.js
@@ -0,0 +1,10 @@
+const calculateSum = require("./sum");
+
+var a = 10;
+
+var b = 20;
+
+const sum = calculateSum(a, b);
+
+console.log(globalThis);
+console.log(sum);
--- practice-1/hello.js
+++
@@ -1,5 +0,0 @@
-const math = require("./math");
-
-console.log("Add => ", math.add(4, 6));
-
-console.log("Subtract => ", math.sub(10, 5));
